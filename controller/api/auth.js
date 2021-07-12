b28e4ddb96a367ea0594e42d06dc35eb8bed2bf4
Pixo Destya Fandy <telbokbatok@gmail.com>
2021-07-12 13:02:36 +0700

    restart sev

--- controller/api/auth.js
+++ controller/api/auth.js
@@ -56,4 +56,19 @@ router.get('/getqr', (req,res) => {
     });
 });
 
+router.get('/restart', (req, res, next) => {
+  setTimeout(function () {
+		// When NodeJS exits
+		process.on("exit", function () {
+
+				require("child_process").spawn(process.argv.shift(), process.argv, {
+						cwd: process.cwd(),
+						detached : true,
+						stdio: "inherit"
+				});
+		});
+		process.exit();
+	}, 1000);
+});
+
 module.exports = router;
--- controller/index.js
+++ controller/index.js
@@ -1,25 +1,9 @@
 const axios = require('axios');
-// const getUrisrv = 'https://pixodestyafandy-cv-admin.herokuapp.com/';
-// const getUrisrv = 'http://127.0.0.1:1337/';
 
 exports.index = function(req, res){
-  // // var array = new Array();
-  // if (!req.session.languages) {
-  //   req.session.languages = "id";
-  // } else {
-  //   req.session.languages
-  // }
-
   let content = {
 		newMessage: []
 	};
-  // return axios.get('/contact/getcontacts')
-  // .then(resp => {
-	// 	console.log(resp);
-  // })
-  // .catch(error => {
-  //   return console.log(error);
-  // });
 	
 	res.render("Index", content);
 }
